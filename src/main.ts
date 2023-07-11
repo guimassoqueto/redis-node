35ec2ac1f5e40780d46b721af257ba34579fe29c
guimassoqueto <gui.massoqueto@gmail.com>
2023-07-11 09:02:17 -0300

    feat: basic redis application

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,21 @@
-console.log("oi")
+import { createClient } from 'redis';
+
+const client = createClient();
+await client.connect();
+
+let loop = true
+
+while(loop) {
+  if (client.isReady) {
+    let pids = await client.get('amazon_pids');
+    if (pids) { 
+      console.log("pid found!")
+      loop = false
+      console.log(JSON.parse(pids))
+      await client.del('amazon_pids')
+      loop = true
+    }
+  } else {
+    await client.connect()
+  }
+}
